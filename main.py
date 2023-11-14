abe9a75d5b520ce0043322caf7fa6af144b47915
KLONE23UK <62775652+KLONE23UK@users.noreply.github.com>
2023-11-14 20:30:00 +0000

    implament theme support

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ app.config["SECRET_KEY"] = "4GShz7"
 db = SQLAlchemy()
 
 
+host = parser.get('WEB','ip')
+port = parser.getint('WEB','port')
+debug = parser.getboolean('WEB','debug')
+
+url = f"http://{host}:{port}"
+
+theme = parser.get('THEME','mode')
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -32,7 +40,7 @@ db.init_app(app)
 # Home page (for logged in users)
 @app.route("/")
 def home():
-	return render_template("home.html") # serving home page, which includes Menu.html with buttons, etc.
+	return render_template("home.html", theme=theme) # serving home page, which includes Menu.html with buttons, etc.
 with app.app_context():
 	db.create_all()
 
@@ -207,13 +215,6 @@ def table_pw_toggle():
 		return "Unable to connect to Table Sensor."
 	except:
 		return "Some error when running function table_toggle."
-
-
-host = parser.get('WEB','ip')
-port = parser.getint('WEB','port')
-debug = parser.getboolean('WEB','debug')
-
-url = f"http://{host}:{port}"
 
 
 # Running server
