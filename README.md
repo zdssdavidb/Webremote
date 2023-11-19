6fbc59a31ded9393dec0f002f3f937d6302349bb
David B <david.brezgun@gmail.com>
2023-11-19 12:55:13 +0000

    Merging changes: added grafana, pihole, few IP cameras, extra style

--- README.md
+++ README.md
@@ -14,37 +14,38 @@ Project to control various IoT devices in the house (WiFi) and the RPi server (D
 
 
 ## Requirements
-_sudo apt-get update && sudo apt-get install python3
-python3 -m pip install flask_
 
-## Features config
-_touch /var/www/html/{project_name}/status_      # for power switching for devices
+You can install required modules with either command below
 
-_touch /var/www/html/{project_name}/color_      # for storing current LED color
+For virtaul enviroment
+```
+python3 -m pip install -r requirements.txt
+```
+
+Without virtual enviroment
+```
+pip install -r requirements.txt
+```
 
 
 ## Usage
 
-1. Configure interface and port in **main.py**
-2. Start flask web server with **python3 main.py**
-3. Use browser to navigate to server address and register new user.
-4. Login and start using.
-
-
-
+1. At present configuration is done manually editing **config.ini*. You can change ip, port and debug options among others.
+2. Start with `python3 main.py` and your browser will open at the specified ip port specified in previous step.
+3. You will need to register a user on first run to get to the dashboard.
 
 
 ## Plans
+
+- Themes
+- Cusomtize Quick buttons dynically
+- Ability to add more devices
+- Get device details
 
 Planning to work on this and make it public, kinda like a template Smart Home hub.
 Examples for powering on/off devices are for Sonoff Basic smart plugs flashed with Tasmota firmware.
 ![image](https://github.com/zdssdavidb/Webremote/assets/58611751/c500e8af-9cf0-4eea-b590-d1427265c0de)
 [Tasmota](https://tasmota.github.io/docs/)
-
-
-
-
-
 
 
 ## Misc
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+##### Webremote #####
+
+from configparser import ConfigParser
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
-import os, random
-# from sklad import *
+import os
+from sklad_functions import sklad as skl
+import webbrowser
+
+parser = ConfigParser()
+parser.read('config.ini')
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SECRET_KEY"] = "4GShz7"
 db = SQLAlchemy()
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -16,170 +25,219 @@ class Users(UserMixin, db.Model):
 	username = db.Column(db.String(250), unique=True, nullable=False)
 	password = db.Column(db.String(250), nullable=False)
 
+
 db.init_app(app)
+
 
 # Home page (for logged in users)
 @app.route("/")
 def home():
-	return render_template("home.html")
-
+	return render_template("home.html") # serving home page, which includes Menu.html with buttons, etc.
 with app.app_context():
 	db.create_all()
+
 
 @login_manager.user_loader
 def loader_user(user_id):
 	return Users.query.get(user_id)
 
-# # Register new user
+
+# I would recommend commenting this block out after 1 user is registered.
+# Register new user
 # @app.route('/register', methods=["GET", "POST"])
 # def register():
-# 	if request.method == "POST":
-# 		user = Users(username=request.form.get("username"),
-# 					password=request.form.get("password"))
-# 		db.session.add(user)
-# 		db.session.commit()
-# 		return redirect(url_for("login"))
-# 	return render_template("sign_up.html")
+#     if request.method == "POST":
+#         user = Users(username=request.form.get("username"),
+#                     password=request.form.get("password"))
+#         db.session.add(user)
+#         db.session.commit()
+#         return redirect(url_for("login"))
+
+#     return render_template("sections/sign_up.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	if request.method == "POST":
-		user = Users.query.filter_by(
-			username=request.form.get("username")).first()
-		if user.password == request.form.get("password"):
-			login_user(user)
-			return redirect(url_for("home"))
-	return render_template("login.html")
+    try:
+        if request.method == "POST":
+            user = Users.query.filter_by(
+                username=request.form.get("username")).first()
+            if user.password == request.form.get("password"):
+                login_user(user)
+                return redirect(url_for("home"))
+        return render_template("sections/login.html")
+    except:
+        return render_template("sections/login.html")
+
 
 # Logout user function
 @app.route("/logout")
 def logout():
-	logout_user()
-	return redirect(url_for("login"))
+    # write_log("Logging Out")
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/devices")
+def devices():
+    return render_template("devices.html")
+
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
 
 # TV Power toggle
 @app.route("/tv_power")
 def tv_power():
-	print("PWD for tv_power", os.getcwd())
-	from sklad import tv_power_toggle
-	tv_power_toggle()
-	return redirect(url_for("home"))
+    try:
+        skl.tv_power_toggle()
+        return redirect(url_for("home"))
+    except:
+        return render_template("error.html")        # probably device timed out, just returning home screen. Will need to find a way to leave feedback to user that the function failed, but without creating a whole new page for it.
+
 
 # TV LED toggle
 @app.route("/tv_led_toggle")
 def tv_led_toggle():
-	try:
-		f = open("status", 'r')
-		data = f.read()
-		f.close()
-		from sklad import tv_led_off, tv_led_default
-		tv_led_off()
-		with open("status", "w") as file:
-			if data == "1":
-				print("LED is ON, turning OFF")
-				tv_led_off()
-				file.write("0")
-				return redirect(url_for("home"))
-			elif data =="0":
-				print("LED is OFF, turning ON")
-				tv_led_default()
-				file.write("1")
-				return redirect(url_for("home"))
-			else:
-				# print("Oops")
-				file.write("0")
-				return redirect(url_for("home"))
-	except:
-		return "tv_led_toggle Function Failed!"
+    try:
+        data = parser.get('TV_LIGHTS', 'status')
+        skl.tv_led_off()
+        with open("config.ini", "w") as configfile:
+            if data == "1":
+                print("LED is ON, turning OFF")
+                skl.tv_led_off()
+                parser.set('TV_LIGHTS','status',str(0))
+                parser.write(configfile)
+                configfile.close()
+                return redirect(url_for("home"))
+            elif data =="0":
+                print("LED is OFF, turning ON")
+                skl.tv_led_default()
+                parser.set('TV_LIGHTS','status',str(1))
+                parser.write(configfile)
+                configfile.close()
+                return redirect(url_for("home"))
+            else:
+                parser.set('TV_LIGHTS','status',str(0))     # scenario 3, forcing lights off.
+                parser.write(configfile)
+                configfile.close()
+                return redirect(url_for("home"))
+    except:
+        return render_template("error.html")
+
 
 # TV LED color shuffle 
 @app.route("/tv_led_shuffle")
 def tv_led_shuffle():
-	from sklad import tv_led_blue, tv_led_yellow, tv_led_green, tv_led_pink, tv_led_default
-	f = open("color", 'r')
-	data = f.read()
-	f.close()
-	with open("color", "w") as file:
-		if data == "DEFAULT":
-			print("LED is Default")
-			tv_led_default()
-			file.write("BLUE")					# WRITE WHAT NEXT COLOR SHOULD BE
-			return redirect(url_for("home"))
-		elif data =="BLUE":
-			print("LED is BLUE")
-			tv_led_yellow()
-			file.write("YELLOW")
-			return redirect(url_for("home"))
-		elif data =="YELLOW":
-			print("LED is YELLOW")
-			tv_led_yellow()
-			file.write("GREEN")
-			return redirect(url_for("home"))
-		elif data =="GREEN":
-			print("LED is GREEN")
-			tv_led_green()
-			file.write("PINK")
-			return redirect(url_for("home"))
-		elif data =="PINK":
-			print("LED is PINK")
-			tv_led_pink()
-			file.write("DEFAULT")
-			return redirect(url_for("home"))
-		# else:
-		# 	print("LED is Default (Force)")
-		# 	tv_led_default()
-		# 	file.write("DEFAULT")
-		# 	return redirect(url_for("home"))
+    data = parser.get('TV_LIGHTS', 'colour')
+    with open("colour", "w") as configfile:
+        if data == "DEFAULT":
+            print("LED is Default")
+            skl.tv_led_default()
+            parser.set('TV_LIGHTS','colour','BLUE')     # write the value of next colour in the line (sklad functions)
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        elif data =="BLUE":
+            print("LED is BLUE")
+            skl.tv_led_blue()
+            parser.set('TV_LIGHTS','colour','YELLOW')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        elif data =="YELLOW":
+            print("LED is YELLOW")
+            skl.tv_led_yellow()
+            parser.set('TV_LIGHTS','colour','GREEN1')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        elif data =="GREEN1":
+            print("LED is GREEN1")
+            skl.tv_led_green1()
+            parser.set('TV_LIGHTS','colour','GREEN2')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        elif data =="GREEN2":
+            print("LED is GREEN2")
+            skl.tv_led_green2()
+            parser.set('TV_LIGHTS','colour','PINK')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        elif data =="PINK":
+            print("LED is PINK")
+            skl.tv_led_pink()
+            parser.set('TV_LIGHTS','colour','DEFAULT')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
+        else:
+            print("LED is Default (Force)")
+            skl.tv_led_default()
+            parser.set('TV_LIGHTS','colour','DEFAULT')
+            parser.write(configfile)
+            configfile.close()
+            return redirect(url_for("home"))
 
 # Cube power toggle
-@app.route("/Cube_power_toggle")
+@app.route("/cube_power_toggle")
 def cube_pw_toggle():
 	try:
-		from sklad import cube_power_toggle
-		cube_power_toggle()
+		skl.cube_power_toggle()
 		return redirect(url_for("home"))
 	except:
-		return "cube_power_toggle Function Failed!"
+		return render_template("error.html")
 
-# Load weather page
-@app.route("/weather")
-def load_weather():
-	#try:
-	return render_template("weather.html")
-	#except:
-	#	return "load_weather Function Failed!"
-
-# get weather data
+# get weather data (serve static page, it gets updated using crontab entries OR a sklad function)
 @app.route('/get_weather', methods = ['GET', 'POST'])
 def get_weather():
-	# try:
-	import time
-	# os.chdir("/home/pi/scripts")
-	print("################# PWD Start of Weather function!", os.getcwd())
-	import sklad
-	from sklad import get_weather_full
-	sklad.get_weather_full()
-	time.sleep(1)
-	# os.chdir("/var/www/html/webremote/templates")
-	print("################# PWD at End of Weather function!", os.getcwd())	
-	return redirect(url_for("home")) 
-	# except:
-	# 	return redirect(url_for("home"))
-	
+    try:
+        return render_template("weather.html")
+    except:
+        return render_template("error.html")
+
+# Grafana Dashboard inside iframe (static html page)
+@app.route('/get_temp', methods = ['GET', 'POST'])
+def get_temp():
+    try:
+        return render_template("grafana.html")
+    except:
+        return render_template("error.html")
+
+# Reset XRDP service (when stuck, on demand)
+@app.route('/res_xrdp', methods = ['GET', 'POST'])
+def res_xrdp():
+    try:
+        os.system("sudo systemctl restart xrdp.service")
+        return redirect(url_for("home"))
+    except:
+        return render_template("error.html")
+
 # Table power toggle
 @app.route("/table_power_toggle")
 def table_pw_toggle():
 	try:
-		from sklad import table_toggle
-		table_toggle()
+		skl.table_toggle()
 		return redirect(url_for("home"))
 	except ConnectionError:
 		return "Unable to connect to Table Sensor."
 	except:
-		return "Some error when running function table_toggle."
+		return render_template("error.html")
+
+
+host = parser.get('WEB','ip')
+port = parser.getint('WEB','port')
+debug = parser.getboolean('WEB','debug')
+
+url = f"http://{host}:{port}"
+
 
 # Running server
 if __name__ == "__main__":
-	app.run(host="192.168.0.234", port=9094, debug=True)
-	# app.run(host="100.79.200.135", port=9094, debug=True)
-
+    if parser.getint('MISC','launch') == 1:
+        webbrowser.open(url,new=2)
+    app.run(host=host, port=port, debug=debug)	
